c015cdd0a5e062968748a8ee63a156e3caa6a091
Elivan Kook <elivanmetal@gmail.com>
2018-04-22 17:39:57 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 //Register event listener
 io.on('connection', (socket) => {
     console.log('New user connected from Server side');
+    //add socket.emit from Admit text welcome to the chat
+    socket.emit('newMessage', { //We will get the new msg from admin
+        from: 'Admin',
+        text: 'Welcome to the Chat App',
+        createdAt: new Date().getTime()
+    });
+    //socket.broadcast.emit from admin text new user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    });
     //Create newMessage event
     // socket.emit('newMessage', {
     //     from: 'max@gmail.com',
@@ -25,11 +37,18 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message) => {
         console.log('The createMessage event', message);
         //io.emit to all users - wired the newMessage event
-        io.emit('newMessage', {
+        io.emit('newMessage', { //we get the newMessage from the user.
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        //Broadcasting Events - all but me will see msg.
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     io.on('disconnetion', (socket) => {
