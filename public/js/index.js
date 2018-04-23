de7b0663bf193ac6867ed517b99efc340e3ba98f
Elivan Kook <elivanmetal@gmail.com>
2018-04-23 12:59:28 +0200

    Using jQuery and added form for messages, display income message in browser

--- public/js/index.js
+++ public/js/index.js
@@ -14,14 +14,32 @@ socket.on('discconet', function() {
 });
 
 //Listen to the newMessage event
+//When a new message arrive, it will be stored in a list item
 socket.on('newMessage', function(message) {
     console.log('Listen from index.js - newMessage', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    //render it to the dom
+    jQuery('#messages').append(li);
 });
 //Event acknowledgment - if the data is valid
 //send from the server back to the client that we got that data (in a callback function).
-socket.emit('createMessage', {
-    from: 'Frank',
-    text: 'Hi'
-}, function(data) {
-    console.log('Got the data', data); //this function will be called on the server side as a callback
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, function(data) { //this callback funtion will be called when the acknowledgemnt arrived at the client 
+//     console.log('Got the data', data); //this function will be called on the server side as a callback
+// });
+//Adding event listener to the form 
+//When user submit form the function execute
+jQuery('#message-form').on('submit', function(eve) {
+    //The eve event variable is to override the default behaivor to refresh page 
+    eve.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    })
 });
